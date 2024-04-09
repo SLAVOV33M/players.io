fc43b565ef741e9c9b5462912b890540492ef82f
SLAVOV33M <116291753+SLAVOV33M@users.noreply.github.com>
2024-04-09 13:42:31 +0100

    Add files via upload

---
+++ player.js
@@ -0,0 +1,138 @@
+var Modelos = [
+	{
+        order: "new",
+        img: "https://i.postimg.cc/dDMWjMDh/modelo-multradio.jpg",
+        model: 17,
+        price: 6000,
+        size: "920 x 756",
+        id: 17
+    },
+	{
+        order: "new",
+        img: "https://i.postimg.cc/P5fBdzfn/modelo-17-html-img-1.jpg",
+        model: 16,
+        price: 5000,
+        size: "830 x 600",
+        id: 16
+    },
+	{
+        order: "new",
+        img: "https://i.postimg.cc/hPRhTdNw/modelo-16-html.jpg",
+        model: 15,
+        price: 5000,
+        size: "650 x 390",
+        id: 15
+    },
+    {
+        order: "new",
+        img: "https://i.postimg.cc/6p8Cy92y/modelo-15-html.jpg",
+        model: 14,
+        price: 5000,
+        size: "620 x 440",
+        id: 14
+    },
+	{
+        order: "html",
+        img: "https://i.postimg.cc/tCRGm5m3/modelo-14-html.jpg",
+        model: 13,
+        price: 4000,
+        size: "720 x 400",
+        id: 13
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/brtVRR2j/modelo-12-html.jpg",
+        model: 12,
+        price: 4000,
+        size: "720 x 720",
+        id: 12
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/GmQTxCkM/modelo-01-html.jpg",
+        model: 1,
+        price: 4000,
+        size: "650 x 450",
+        id: 1
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/t4yV6TVZ/modelo-02-html.jpg",
+        model: 2,
+        price: 4000,
+        size: "650 x 450",
+        id: 2
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/R0p32LpK/modelo-03-html.jpg",
+        model: 3,
+        price: 4000,
+        size: "520 x 470",
+        id: 3
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/gcZwBRVL/modelo-04-html.jpg",
+        model: 4,
+        price: 4000,
+        size: "340 x 520",
+        id: 4
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/0QLzy2sN/modelo-05-html.jpg",
+        model: 5,
+        price: 8000,
+        size: "100% x 880",
+        id: 5
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/7YrCv69s/modelo-06-html.jpg",
+        model: 6,
+        price: 8000,
+        size: "100% x 880",
+        id: 6
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/vmZgPNhG/modelo-07-html.jpg",
+        model: 7,
+        price: 8000,
+        size: "100% x 880",
+        id: 7
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/qqyzhVJx/modelo-08-html.jpg",
+        model: 8,
+        price: 4000,
+        size: "610 x 575",
+        id: 8
+    },
+    {
+        order: "html",
+        img: "https://i.postimg.cc/50HHJQDG/modelo-10-html.jpg",
+        model: 9,
+        price: 4000,
+        size: "600 x 482",
+        id: 9
+    },
+	{
+        order: "html",
+        img: "https://i.postimg.cc/5yWmdjc1/modelo-11-html.jpg",
+        model: 10,
+        price: 4000,
+        size: "350 x 520",
+        id: 10
+    },
+	{
+        order: "html",
+        img: "https://i.postimg.cc/gcX7dGtB/modelo-13-html.jpg",
+        model: 11,
+        price: 4000,
+        size: "360 x 540",
+        id: 11
+    }
+];
